91e485f51fc9b452855dc6d46e1745a23d61b519
liamweston <liam.weston@dotabove.com>
2020-02-23 15:31:53 +0000

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "eu-west-2",
+      BUCKET: "squirrel-uploads"
+    },
+    apiGateway: {
+      REGION: "eu-west-2",
+      URL: "https://na6alr2823.execute-api.eu-west-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "eu-west-2",
+      USER_POOL_ID: "eu-west-2_Cs2XwiBnf",
+      APP_CLIENT_ID: "69dja1sdlgjfa6pka7uss6k8kp",
+      IDENTITY_POOL_ID: "eu-west-2:037dcf62-7826-4d45-aaac-b49031781a29"
+    }
+  };
