45126bac172de76ff4c7725ce43ce28868cdfc7b
liamweston <liam.weston@dotabove.com>
2020-03-02 23:18:43 +0000

    Updating the lander

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
 import "./Home.css";
 import { API } from "aws-amplify";
+import { Link } from "react-router-dom";
 
 import { LinkContainer } from "react-router-bootstrap";
 
@@ -55,8 +56,16 @@ export default function Home(props) {
   function renderLander() {
     return (
       <div className="lander">
-        <h1>entrusted.to</h1>
-        <p>Because it's too late to tell them when you're gone...</p>
+        <h1>Scratch</h1>
+        <p>A simple note taking app</p>
+        <div>
+          <Link to="/login" className="btn btn-info btn-lg">
+            Login
+          </Link>
+          <Link to="/signup" className="btn btn-success btn-lg">
+            Signup
+          </Link>
+        </div>
       </div>
     );
   }
